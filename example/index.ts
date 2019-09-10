1da8b5f9becca67fbdde726c75d174dfb7c878bd
Michael Warner <MichaelWarner@EuropaSports.com>
2019-09-10 09:41:13 -0400

    removed product test

--- example/index.ts
+++ example/index.ts
@@ -1,6 +1,5 @@
 import {store, rootState} from './store'
 import cart from './features/cart'
-import products from './features/products'
 
 
 const unsubscribe = store.subscribe(() => {
@@ -9,7 +8,6 @@ const unsubscribe = store.subscribe(() => {
   console.log(state.cart.counter, state.cart.double, state.cart.half)
 })
 
-products.action.fetchProducts(null)
 cart.action.someAsyncAction(50000)
 cart.commit.addCounter(3)
 cart.commit.addCounter(20)
--- example/slices.ts
+++ example/slices.ts
@@ -1,10 +1,8 @@
 import RedVue from '../RedVue';
 import cart from './features/cart'
-import products from './features/products'
 
 const slices = RedVue.combineSlices({
-  cart: cart.slice.reducer,
-  products: products.slice.reducer
+  cart: cart.slice.reducer
 })
 
 export type rootState = ReturnType<typeof slices>
