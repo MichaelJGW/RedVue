36afca9bc31a9373bf706b72a19c86b37b298fd9
Michael Warner <MichaelWarner@EuropaSports.com>
2019-09-11 13:18:14 -0400

    auto prevent middleware from calling itself

--- src/examples/fullExample/middleware/logger.ts
+++ src/examples/fullExample/middleware/logger.ts
@@ -2,11 +2,9 @@ import { logger as loggerAction } from './../features/logger';
 import { middleware } from 'RedVue';
 
 export const logger = middleware((action) => {
-    if (action.type !== 'logger/insertLog') {
-        loggerAction.commit.insertLog({
-            timestamp: new Date(),
-            action: action.type,
-            payload: action.payload
-        })
-    }
+    loggerAction.commit.insertLog({
+        timestamp: new Date(),
+        action: action.type,
+        payload: action.payload
+    })
 })
