f86ff873de667bfce77023b5fe34157a6ebe559b
Michael Warner <MichaelWarner@EuropaSports.com>
2019-09-13 16:07:11 -0400

    turnned the tests back on

--- src/examples/fullExample/index.ts
+++ src/examples/fullExample/index.ts
@@ -8,22 +8,22 @@ const unsubscribe = store.subscribe(() => {
   console.log(state.counter.count, state.cart.counter, state.cart.double, state.cart.half)
 })
 
-// cart.action.someAsyncAction(50000)
-// cart.commit.addCounter(3)
-// cart.commit.addCounter(20)
+cart.action.someAsyncAction(50000)
+cart.commit.addCounter(3)
+cart.commit.addCounter(20)
 cart.commit.addCounter(1)
 
-// // Turn off Logger for Speed Testing
-// logger.commit.stop()
+// Turn off Logger for Speed Testing
+logger.commit.stop()
 
-// // Speed Test
-// setTimeout (() => {
-//   unsubscribe()
-//   const testAmount = 10000;
-//   const startTime = new Date()
-//   for (let index = 0; index < testAmount; index++) { cart.commit.addCounter(1); }
-//   const deltaTime = new Date().getTime() - startTime.getTime()
-//   console.log(`time elapsed ${deltaTime}ms for ${testAmount} commits. About ${deltaTime / testAmount}ms for each commit.`)
-// }, 1000)
+// Speed Test
+setTimeout (() => {
+  unsubscribe()
+  const testAmount = 10000;
+  const startTime = new Date()
+  for (let index = 0; index < testAmount; index++) { cart.commit.addCounter(1); }
+  const deltaTime = new Date().getTime() - startTime.getTime()
+  console.log(`time elapsed ${deltaTime}ms for ${testAmount} commits. About ${deltaTime / testAmount}ms for each commit.`)
+}, 1000)
 
 console.log(state)
